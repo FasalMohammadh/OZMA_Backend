d9128887f504325691fb82af06a2ae7641196369
fasalmohammadh <fazalmohammed9367@gmail.com>
2023-02-25 13:40:26 +0530

    updates

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,15 @@ import cors from 'cors';
 import userRoutes from './Routers/userRouter.js';
 
 const app = express();
+app.all('*', function (req, res) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type,Content-Length, Authorization, Accept,X-Requested-With'
+  );
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+});
+
 app.use(cors());
 app.use(express.json());
 app.use('/user', userRoutes);
